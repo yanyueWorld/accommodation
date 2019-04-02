0b6b9617f7e202507ab1602d547e159f006d62d7
yanyueWorld <1361557116@qq.com>
2019-04-02 16:39:27 +0800

    添加git测试

---
+++ src/test/java/com/example/yanyue/service/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) .
+ * All Rights Reserved.
+ */
+package com.example.yanyue.service;
+
+/**
+ * Test.
+ *
+ * @author yanyue, 2019/4/2
+ * @version ORAS v1.0
+ */
+public class Test
+{
+}
