be2d21447c6b3899c743324e9b6d9e0cfd03cb03
yanyueWorld <1361557116@qq.com>
2019-04-09 12:10:28 +0800

    项目更改，前后端完全分离

---
+++ src/main/java/com/example/yanyue/component/CorsFilter.java
@@ -0,0 +1,24 @@
+package com.example.yanyue.component;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-type");
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        System.out.println("*********************************过滤器被使用**************************");
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+}
